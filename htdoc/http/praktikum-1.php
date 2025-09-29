81e01f41e51532e55e709e79e368a99b36c6a8e6
qulDev <mm.rizzqullah@gmail.com>
2025-09-29 08:34:11 +0800

    Add PHP script to fetch and display city data from API

---
+++ htdoc/http/praktikum-1.php
@@ -0,0 +1,20 @@
+<?php
+$curl = curl_init();
+
+
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://rajaongkir.komerce.id/api/v1/destination/city/1",
+    CURLOPT_HTTPHEADER => ["accept: application/json","key: 8F3pm71P955d0ab5aea9e6e8IdRXmHne"],
+    CURLOPT_RETURNTRANSFER => true,
+]);
+
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+$data=json_decode($response,true);
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+?>
